b80580442fdefb6d8150eb00019b31b7e8c5c828
Jay Jain <jay4codes@gmail.com>
2022-11-20 19:34:32 +0530

    data added

---
+++ src/data/resume.js
@@ -0,0 +1,148 @@
+export const skills = {
+  languages: ["Python", "C++", "SQL", "JavaScript", "Java", "Prolog"],
+  development: [
+    "AWS",
+    "React.js",
+    "Django",
+    "Postman",
+    "Node.js",
+    "GCP",
+    "Firebase",
+    "Flask",
+    "CSS",
+    "HTML",
+  ],
+  databases: ["MySQL", "NoSQL", "GraphQL", "MongoDB"],
+  data_science: [
+    "Excel",
+    "Tableau",
+    "Pandas",
+    "NumPy",
+    "Scikit-Learn",
+    "Matplotlib",
+    "Plotly",
+    "D3.js",
+  ],
+  machine_learning: [
+    "Computer Vision",
+    "TensorFlow",
+    "Time Series Analysis",
+    "CNN",
+    "Deep Learning",
+    "Artifical Intelligence",
+  ],
+  others: [
+    "Git",
+    "GitHub",
+    "Figma",
+    "Netlify",
+    "Vercel",
+    "Heroku",
+    "Streamlit",
+    "Adobe After Effects",
+    "Adobe Photoshop",
+    "Blender",
+  ],
+  icon: [""],
+};
+
+export const projects = {
+  name: [
+    "Baker Hughes: Data Visualization",
+    "VLearn: Visualizing Algorithms",
+    "EnhanceAI: Enhancing Images",
+    "VCare: Using AI for Fitness",
+    "Adobe: Analytics Challenge",
+    "Air Touch: Virtual Mouse",
+  ],
+  image: [""],
+  stack: [
+    "Streamlit | Python | Matplotlib | Plotly | PyDeck | Excel",
+    "ReactJS | ChartJS | Firebase",
+    "OpenCV | CNN | Python | REST API | Streamlit",
+    "ReactJS | TensorFlowJS | Firebase",
+    "Customer Journey Analytics | Data Science | Python | Tableau",
+    "OpenCV | Object Detection | C++ | Python | JavaScript Browser Extension",
+  ],
+  desc: [
+    "Developed an interactive data visualization dashboard and analysis for Baker Hughes Gas Turbine data. Created insightful visualizations using python libraries including matplotlib, plotly, streamlit, pydeck, etc.",
+    "A website to visualize algorithms like sorting, decision trees, neural network architectures, etc. The website also comprises links to upcoming competitions and mock AI interviews along with a student progress dashboard.",
+    "An image editing website that along with the usual image editing also provides Low Light Enhancement, Denoising & Upscaling. The website uses a RESTful API created using Django to fetch the enhanced images from a CNN model.",
+    "A fitness website that uses a Deep Learning model for posture detection that identifies postures from users' webcams and helps them correct their yoga postures. The website also comprised diet plans, live video sessions with trainers, and a user progress dashboard.",
+    "A complete data analysis, visualization, and storytelling for real-time Hilton Hotels UX data on Adobe's Customer Journey Analytics platform to fetch useful insights to promote Hilton's digital services.",
+    "Built a virtual mouse that performs traditional mouse operations using hand gestures.",
+  ],
+  link: [""],
+  source: [""],
+};
+
+export const works = {
+  role: ["Cloud Backend Developer Intern", "Vice-Chairperson (Technical)"],
+  image: [""],
+  company: ["CitJo", "S4DS"],
+  link: ["https://www.citjo.in", "https://djss4ds.in/"],
+  desc: [
+    "Deployed CI/CD pipeline for a local news-sharing app's backend on AWS & GitHub. Developed a sustainable cloud architecture for the cross-platform Flutter app using AWS Amplify, S3, Cognito, DynamoDB, AppSync, and GraphQL.",
+    "Led a team of 80+ students to organize numerous events that comprised pan-India workshops and hackathons to promote Data Science and AI. Co-Hosted the 6th International Conference of ICDMAI and led the conference's promotional creatives.",
+  ],
+  duration: ["Jul 2022 - Oct 2022", "Oct 2021 - Present"],
+  location: ["Mumbai, India", "Mumbai, India"],
+};
+
+export const education = {
+  institute: [
+    "Dwarkadas J. Sanghvi College of Engineering",
+    "Navneet Education Society",
+    "Christ Church School",
+  ],
+  grade: ["9.44/10", "89%", "96%"],
+  degree: [
+    "Bachelor of Computer Science & Engineering (Data Science)",
+    "12th HSC",
+    "10th ICSE",
+  ],
+  image: [""],
+  link: [""],
+  duration: ["2021 - Present", "2018 - 2020", "2008 - 2018"],
+  location: ["Mumbai, India", "Mumbai, India", "Mumbai, India"],
+};
+
+export const achievements = {
+  prize: [
+    "3rd Place at Baker Hughes Data Viz Challenge",
+    "Winner at KJSCE Hack 6.0",
+    "Best UI/UX at HackOver 3.0",
+    "Rank 1 SOF Cyber Olympiad 15.0",
+  ],
+  author: [
+    "TAMU Datathon",
+    "KJSCE CodeCell",
+    "GDSC CRCE",
+    "Science Olympiad Foundation",
+  ],
+  link: [""],
+  image: [""],
+  desc: [
+    "Placed 2nd Runner-up from over 200 teams in the 24-hour Datathon. Developed interactive data analysis & visualizations dashboard using python libraries including streamlit, pandas, matplotlib, plotly, pydeck, etc. for the Baker Hughes Gas Turbine dataset",
+    "Secured 1st place from over 400 teams in the 24-hour open innovation hackathon. Built a website prototype that visualizes sorting algorithms and also comprised quizzes, upcoming competitions, and mock AI interviews along with a student progress dashboard.",
+    "Won the best UI/UX prize from over 150 teams at the 24-hour hackathon",
+    "1st Rank secured in Mumbai district, 8th state rank in Maharashtra and 64th national rank",
+  ],
+};
+
+export const courses = {
+  name: [
+    "AWS Cloud Foundations",
+    "AWS Machine Learning Foundations",
+    "Google Data Analytics",
+    "Deep Learning Specialization",
+  ],
+  author: ["AWS Academy", "AWS Academy", "Google", "DeepLearningAI"],
+  desc: [
+    "Gained an understanding of AWS Cloud Architecture and its services",
+    "Deployed machine learning environments in AWS using EC2 & Sagemaker",
+    "Understood Concepts of Data analytics and visualization",
+    "Acquired knowledge about different neural networks and deep learning concepts",
+  ],
+  date: ["Jun 2022", "May 2022", "Apr 2022", "Jan 2022"],
+};
